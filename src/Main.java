a3718cf0b874e6d6b41c04e3ca4b47f3a7fee608
Sveta <sveta.zheleznova.03@mail.ru>
2022-11-21 00:24:45 +0300

    4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +21,15 @@ public class Main {
         System.out.println(toCamelCase("is_modal_open"));
         System.out.println(toSnakeCase("helloEdabit"));
         System.out.println(toSnakeCase("getColor"));
+
+        System.out.println("Задание 4");
+        double[] array1 = new double[]{9, 17, 30, 1.5};
+        double[] array2 = new double[]{16, 18, 30, 1.8};
+        double[] array3 = new double[]{13.25, 15, 30, 1.5};
+        System.out.println(overTime(array1));
+        System.out.println(overTime(array2));
+        System.out.println(overTime(array3));
+
 
     }
 
@@ -107,5 +117,37 @@ public class Main {
         }
         result.append(str);
         return result;
+    }
+
+    // вычисляем сверхурочную работу и оплату, связанную с сверхурочной работой
+    //Работа с 9 до 5: обычные часы работы
+    //После 5 вечера - сверхурочная работа
+    public static String overTime(double[] arr) {
+        double start = arr[0]; // Начало рабочего дня
+        double finish = arr[1]; // Конец рабочего дня.
+        double hourlyRate = arr[2]; // Почасовая ставка
+        double overtimePayFactor = arr[3]; // Множитель сверхурочных работ
+        double payment = 0;
+
+        if (start >= 9 && start <= 17 && finish <= 17){
+            // если работал в промежуток с 9 до 17
+            payment = (finish - start) * hourlyRate;
+        }else if (start > 17 && finish > 17){
+            // если начал работать после 17, закончил до 12 ночи
+            payment = (finish - 17) * hourlyRate * overtimePayFactor;
+        }else if (start > 17 && finish < 9) {
+            // если начал работать после 17, работал ночью, закончил до 9 утра
+            // 24 - 17 -> до полуночи + finish -> от полуночи до момента, когда закончил
+            payment = (24 - 17 + finish) * hourlyRate * overtimePayFactor;
+        } else if (start <= 17 && finish > 17){
+            // если начал работать до 17, закончил до 12 ночи
+            payment = (17 - start) * hourlyRate +
+                    (finish - 17) * hourlyRate * overtimePayFactor;
+        } else if (start <= 17 && finish < 9){
+            // если начал работать до 17, работал ночью, закончил до 9 утра
+            payment = (17 - start) * hourlyRate +
+                    (24 - 17 + finish) * hourlyRate * overtimePayFactor;
+        }
+        return "$" + String.format("%.2f", payment); // округляем до сотой
     }
 }
